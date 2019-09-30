1c9f97fc90f96fa29074a4e51912a19a84912145
Jelani Woods <jelani@firstdraft.com>
2019-09-30 15:26:56 -0500

    using forms and states and props and shit

--- src/components/CarTool.js
+++ src/components/CarTool.js
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const CarTool = () => {
+export const CarTool = (props) => {
+
+  const [ carForm, setCarForm ] = useState({
+    make: '',
+    model: '',
+    year: 1900,
+    color: '',
+    price: 0,
+  });
+
+  const [ cars, setCars ] = useState(props.cars.concat());
+
+  const change = (e) => {
+    setCarForm({
+      ...carForm,
+      [e.target.name]: e.target.type === 'number'
+      ? Number(e.target.value) : e.target.value,
+    });
+  };
+
+  const addCar = () => {
+    setCars(cars.concat({
+      ...carForm,
+      id: Math.max(...cars.map(c => c.id)) + 1,
+    }));
+
+    setCarForm({
+      make: '',
+      model: '',
+      year: 1900,
+      color: '',
+      price: 0,
+    })
+  }
 
   return (
   <>
@@ -19,24 +52,45 @@ export const CarTool = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Ford</td>
-          <td>Fusion</td>
-          <td>2018</td>
-          <td>Silver</td>
-          <td>30000</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Hundai</td>
-          <td>Camero</td>
-          <td>1907</td>
-          <td>Gold</td>
-          <td>1000</td>
-        </tr>
+       {cars.map(car => <tr key={car.id}>
+         <td>{car.id}</td>
+         <td>{car.make}</td>
+         <td>{car.model}</td>
+         <td>{car.year}</td>
+         <td>{car.color}</td>
+         <td>{car.price}</td>
+       </tr>)}
       </tbody>
     </table>
+
+    <form>
+      <div>
+        <label htmlFor="make-input">Make:</label>
+        <input id="make-input" type="text" name="make"
+          value={carForm.make} onChange={change}/>
+      </div>
+      <div>
+        <label htmlFor="model-input">Model:</label>
+        <input id="model-input" type="text" name="model"
+         value={carForm.model} onChange={change} />
+      </div>
+      <div>
+        <label htmlFor="year-input">Year:</label>
+        <input id="year-input" type="number" name="year"
+          value={carForm.year} onChange={change} />
+      </div>
+      <div>
+        <label htmlFor="color-input">Color:</label>
+        <input id="color-input" type="text" name="color"
+         value={carForm.color} onChange={change} />
+      </div>
+      <div>
+        <label htmlFor="price-input">Price:</label>
+        <input id="price-input" type="number" name="price"
+         value={carForm.price} onChange={change} />
+      </div>
+      <button type="button" onClick={addCar}>Add Car</button>
+    </form>
   </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { CarTool } from './components/CarTool'
+const cars = [
+  {id: 1, make: "Ford", model: "Fusion", year: 2018, color: "Silver", price: 30000},
+  {id: 2, make: 'Hundai', model: 'Camero', year: 1907, color: 'Gold', price: 1000},
+];
 
 ReactDOM.render(
-  React.createElement(CarTool),
+  <CarTool cars={cars} />,
   document.querySelector('#root'),
   
 );
